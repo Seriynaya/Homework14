a6ef93e97d1bd35cf89f26a50a3c2dc42d0ce77e
Nagaeva Elina <05batman@mail.ru>
2024-10-14 21:33:52 +0400

    Создание функции чтения JSON-файла и создания объекта классов.

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import json
+
+from src.product import Product
+from src.category import Category
+
+
+def read_json_file(path: str) -> dict:
+    with open(path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+def obj_from_json(data):
+    categories = []
+    for category in data:
+        products = []
+        for product in category['products']:
+            products.append(Product(**product))
+        category['products'] = products
+        categories.append(Category(**category))
+
+    return categories
+
+
+if __name__ == '__main__':
+    read_file = read_json_file("../data/products.json")
+    json_product = obj_from_json(read_file)
+    print(json_product)
